42af18eaa354c1f010a1dbbedca52d14e772b35a
Viktor Stepanov <vitya.zap@gmail.com>
2022-12-16 00:01:04 +0200

    Use map for Header lists
    Fix text casing inconsistencies

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link, withRouter } from 'react-router-dom';
-import styles from './Header.module.sass';
 import CONSTANTS from '../../constants';
+import HEADER_CONSTANTS from 'headerConstants';
 import { clearUserStore } from '../../store/slices/userSlice';
 import Logo from 'components/Logo';
+import ProfileNavBar from './ProfileNavBar/ProfileNavBar';
+import NavList from './NavList/NavList';
+import styles from './Header.module.sass';
 
 class Header extends React.Component {
   logOut = () => {
@@ -35,37 +38,7 @@ class Header extends React.Component {
               src={`${CONSTANTS.STATIC_IMAGES_PATH}menu-down.png`}
               alt="menu"
             />
-            <ul>
-              <li>
-                <Link to="/dashboard" style={{ textDecoration: 'none' }}>
-                  <span>View Dashboard</span>
-                </Link>
-              </li>
-              <li>
-                <Link to="/account" style={{ textDecoration: 'none' }}>
-                  <span>My Account</span>
-                </Link>
-              </li>
-              <li>
-                <Link
-                  to="http:/www.google.com"
-                  style={{ textDecoration: 'none' }}
-                >
-                  <span>Messages</span>
-                </Link>
-              </li>
-              <li>
-                <Link
-                  to="http:/www.google.com"
-                  style={{ textDecoration: 'none' }}
-                >
-                  <span>Affiliate Dashboard</span>
-                </Link>
-              </li>
-              <li>
-                <button onClick={this.logOut}>Logout</button>
-              </li>
-            </ul>
+            <ProfileNavBar list={HEADER_CONSTANTS.PROFILE_NAVBAR} logOut={this.logOut} />
           </div>
           <img
             src={`${CONSTANTS.STATIC_IMAGES_PATH}email.png`}
@@ -113,142 +86,7 @@ class Header extends React.Component {
           <Logo />
           <div className={styles.leftNav}>
             <div className={styles.nav}>
-              <ul>
-                <li>
-                  <span>NAME IDEAS</span>
-                  <img
-                    src={`${CONSTANTS.STATIC_IMAGES_PATH}menu-down.png`}
-                    alt="menu"
-                  />
-                  <ul>
-                    <li>
-                      <a href="http://www.google.com">Beauty</a>
-                    </li>
-                    <li>
-                      <a href="http://www.google.com">Consulting</a>
-                    </li>
-                    <li>
-                      <a href="http://www.google.com">E-Commerce</a>
-                    </li>
-                    <li>
-                      <a href="http://www.google.com">Fashion & Clothing</a>
-                    </li>
-                    <li>
-                      <a href="http://www.google.com">Finance</a>
-                    </li>
-                    <li>
-                      <a href="http://www.google.com">Real Estate</a>
-                    </li>
-                    <li>
-                      <a href="http://www.google.com">Tech</a>
-                    </li>
-                    <li className={styles.last}>
-                      <a href="http://www.google.com">More Categories</a>
-                    </li>
-                  </ul>
-                </li>
-                <li>
-                  <span>CONTESTS</span>
-                  <img
-                    src={`${CONSTANTS.STATIC_IMAGES_PATH}menu-down.png`}
-                    alt="menu"
-                  />
-                  <ul>
-                    <li>
-                      <a href="http://www.google.com">HOW IT WORKS</a>
-                    </li>
-                    <li>
-                      <a href="http://www.google.com">PRICING</a>
-                    </li>
-                    <li>
-                      <a href="http://www.google.com">AGENCY SERVICE</a>
-                    </li>
-                    <li>
-                      <a href="http://www.google.com">ACTIVE CONTESTS</a>
-                    </li>
-                    <li>
-                      <a href="http://www.google.com">WINNERS</a>
-                    </li>
-                    <li>
-                      <a href="http://www.google.com">LEADERBOARD</a>
-                    </li>
-                    <li className={styles.last}>
-                      <a href="http://www.google.com">BECOME A CREATIVE</a>
-                    </li>
-                  </ul>
-                </li>
-                <li>
-                  <span>Our Work</span>
-                  <img
-                    src={`${CONSTANTS.STATIC_IMAGES_PATH}menu-down.png`}
-                    alt="menu"
-                  />
-                  <ul>
-                    <li>
-                      <a href="http://www.google.com">NAMES</a>
-                    </li>
-                    <li>
-                      <a href="http://www.google.com">TAGLINES</a>
-                    </li>
-                    <li>
-                      <a href="http://www.google.com">LOGOS</a>
-                    </li>
-                    <li className={styles.last}>
-                      <a href="http://www.google.com">TESTIMONIALS</a>
-                    </li>
-                  </ul>
-                </li>
-                <li>
-                  <span>Names For Sale</span>
-                  <img
-                    src={`${CONSTANTS.STATIC_IMAGES_PATH}menu-down.png`}
-                    alt="menu"
-                  />
-                  <ul>
-                    <li>
-                      <a href="http://www.google.com">POPULAR NAMES</a>
-                    </li>
-                    <li>
-                      <a href="http://www.google.com">SHORT NAMES</a>
-                    </li>
-                    <li>
-                      <a href="http://www.google.com">INTRIGUING NAMES</a>
-                    </li>
-                    <li>
-                      <a href="http://www.google.com">NAMES BY CATEGORY</a>
-                    </li>
-                    <li>
-                      <a href="http://www.google.com">VISUAL NAME SEARCH</a>
-                    </li>
-                    <li className={styles.last}>
-                      <a href="http://www.google.com">SELL YOUR DOMAINS</a>
-                    </li>
-                  </ul>
-                </li>
-                <li>
-                  <span>Blog</span>
-                  <img
-                    src={`${CONSTANTS.STATIC_IMAGES_PATH}menu-down.png`}
-                    alt="menu"
-                  />
-                  <ul>
-                    <li>
-                      <a href="http://www.google.com">ULTIMATE NAMING GUIDE</a>
-                    </li>
-                    <li>
-                      <a href="http://www.google.com">
-                        POETIC DEVICES IN BUSINESS NAMING
-                      </a>
-                    </li>
-                    <li>
-                      <a href="http://www.google.com">CROWDED BAR THEORY</a>
-                    </li>
-                    <li className={styles.last}>
-                      <a href="http://www.google.com">ALL ARTICLES</a>
-                    </li>
-                  </ul>
-                </li>
-              </ul>
+              <NavList list={HEADER_CONSTANTS.HEADER_LIST} />
             </div>
             {this.props.data && this.props.data.role !== CONSTANTS.CREATOR && (
               <div
