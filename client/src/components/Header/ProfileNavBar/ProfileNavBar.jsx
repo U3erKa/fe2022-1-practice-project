e000e8497fa90c0d1ee47339abd3d375b9659166
Viktor Stepanov <vitya.zap@gmail.com>
2022-12-16 23:29:37 +0200

    Split Header styles

--- client/src/components/Header/ProfileNavBar/ProfileNavBar.jsx
+++ client/src/components/Header/ProfileNavBar/ProfileNavBar.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import styles from '../Header.module.sass'
+import styles from './ProfileNavBar.module.sass'
 
 export default function ProfileNavBar({ list, logOut }) {
   const mapList = list.map(({ id, href, text }) => (
     <li key={id} className={styles.navListItem}>
-      <Link to={href} style={{ textDecoration: 'none' }}>
-        <span>{text}</span>
+      <Link className={styles.navListLink} to={href} style={{ textDecoration: 'none' }}>
+        <span className={styles.navListLinkText}>{text}</span>
       </Link>
     </li>
   ));
@@ -15,7 +15,7 @@ export default function ProfileNavBar({ list, logOut }) {
     <ul className={styles.navList}>
       {mapList}
       <li className={styles.navListItem}>
-        <button onClick={logOut}>Logout</button>
+        <button className={styles.navListBtn} onClick={logOut}>Logout</button>
       </li>
     </ul>
   );
