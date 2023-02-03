53f50c3048a9eeef0cf169d5fa3d207a38e7f8b6
Kyrylo <fresh.js.ftd.@gmail.com>
2023-02-03 20:50:50 +0200

    Add dotenv

--- server/src/seeders/123123123-Bank.js
+++ server/src/seeders/123123123-Bank.js
@@ -1,21 +1,31 @@
+const {
+  SQUADHELP_BANK_NUMBER,
+  SQUADHELP_BANK_NAME,
+  SQUADHELP_BANK_EXPIRY,
+  SQUADHELP_BANK_CVC,
+} = require('../constants');
+
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('Banks', [
-      {
-        cardNumber: '4564654564564564',
-        name: 'SquadHelp',
-        expiry: '11/22',
-        cvc: '453',
-        balance: 0,
-      },
-      {
-        cardNumber: '4111111111111111',
-        name: 'yriy',
-        expiry: '09/23',
-        cvc: '505',
-        balance: 5000,
-      },
-    ], {});
+    return queryInterface.bulkInsert(
+      'Banks',
+      [
+        {
+          cardNumber: SQUADHELP_BANK_NUMBER,
+          name: SQUADHELP_BANK_NAME,
+          expiry: SQUADHELP_BANK_EXPIRY,
+          cvc: SQUADHELP_BANK_CVC,
+          balance: 0,
+        },
+        {
+          cardNumber: '4111111111111111',
+          name: 'yriy',
+          expiry: '09/23',
+          cvc: '505',
+          balance: 5000,
+        },
+      ],
+      {}
+    );
   },
-
 };
