0e78c7e599a59ee814b2496ddf99f5232da786ea
Kyrylo <fresh.js.ftd.@gmail.com>
2023-02-06 21:00:38 +0200

    Fix front files uploading

--- client/src/components/InputComponents/FieldFileInput/FieldFileInput.jsx
+++ client/src/components/InputComponents/FieldFileInput/FieldFileInput.jsx
@@ -1,39 +1,38 @@
 import React from 'react';
-import { Field } from 'formik';
+import { useField } from 'formik';
 
-const FieldFileInput = ({ classes, ...rest }) => {
+const FieldFileInput = ({ classes, name, ...rest }) => {
   const { fileUploadContainer, labelClass, fileNameClass, fileInput } = classes;
 
+  const [{ value, ...restField }, meta, helpers] = useField(name);
+  const getFileName = () => {
+    if (value) {
+      return value.name;
+    }
+    return '';
+  };
+
+  const onChange = (e) => {
+    const file = e.target.files[0];
+    helpers.setValue(file, false);
+  };
+
   return (
-    <Field name={rest.name}>
-      {props => {
-        const { field } = props;
-
-        const getFileName = () => {
-          if (props.field.value) {
-            return props.field.value.name;
-          }
-          return '';
-        };
-
-        return (
-          <div className={fileUploadContainer}>
-            <label htmlFor='fileInput' className={labelClass}>
-              Choose file
-            </label>
-            <span id='fileNameContainer' className={fileNameClass}>
-              {getFileName()}
-            </span>
-            <input
-              {...field}
-              className={fileInput}
-              id='fileInput'
-              type='file'
-            />
-          </div>
-        );
-      }}
-    </Field>
+    <div className={fileUploadContainer}>
+      <label htmlFor="fileInput" className={labelClass}>
+        Choose file
+      </label>
+      <span id="fileNameContainer" className={fileNameClass}>
+        {getFileName()}
+      </span>
+      <input
+        {...restField}
+        className={fileInput}
+        onChange={onChange}
+        id="fileInput"
+        type="file"
+      />
+    </div>
   );
 };
 
--- client/src/components/InputComponents/ImageUpload/ImageUpload.jsx
+++ client/src/components/InputComponents/ImageUpload/ImageUpload.jsx
@@ -2,17 +2,17 @@ import React from 'react';
 import classNames from 'classnames';
 import { useField } from 'formik';
 
-const ImageUpload = props => {
-  const [field, meta, helpers] = useField(props.name);
+const ImageUpload = (props) => {
+  const [{ value, ...restField }, meta, helpers] = useField(props.name);
   const { uploadContainer, inputContainer, imgStyle } = props.classes;
-  const onChange = e => {
+  const onChange = (e) => {
     const node = window.document.getElementById('imagePreview');
     const file = e.target.files[0];
     const imageType = /image.*/;
     if (!file.type.match(imageType)) {
       e.target.value = '';
     } else {
-      field.onChange(file);
+      helpers.setValue(file, false);
       const reader = new FileReader();
       reader.onload = () => {
         node.src = reader.result;
@@ -25,18 +25,18 @@ const ImageUpload = props => {
       <div className={inputContainer}>
         <span>Support only images (*.png, *.gif, *.jpeg)</span>
         <input
-          {...field}
-          id='fileInput'
-          type='file'
-          accept='.jpg, .png, .jpeg'
+          {...restField}
+          id="fileInput"
+          type="file"
+          accept=".jpg, .png, .jpeg"
           onChange={onChange}
         />
-        <label htmlFor='fileInput'>Chose file</label>
+        <label htmlFor="fileInput">Chose file</label>
       </div>
       <img
-        id='imagePreview'
-        className={classNames({ [imgStyle]: !!field.value })}
-        alt='user'
+        id="imagePreview"
+        className={classNames({ [imgStyle]: !!value })}
+        alt="user"
       />
     </div>
   );
