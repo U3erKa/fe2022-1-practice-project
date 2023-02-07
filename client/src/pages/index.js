aad20f7b66aad058e01738b546f8846381ea040e
Viktor Stepanov <vitya.zap@gmail.com>
2023-02-07 12:50:33 +0200

    Use reexport for pages

---
+++ client/src/pages/index.js
@@ -0,0 +1,9 @@
+export { default as ContestCreationPage } from './ContestCreationPage/ContestCreationPage';
+export { default as ContestPage } from './ContestPage/ContestPage';
+export { default as Dashboard } from './Dashboard/Dashboard';
+export { default as Home } from './Home/Home';
+export { default as LoginPage } from './LoginPage/LoginPage';
+export { default as Payment } from './Payment/Payment';
+export { default as RegistrationPage } from './RegistrationPage/RegistrationPage';
+export { default as StartContestPage } from './StartContestPage/StartContestPage';
+export { default as UserProfile } from './UserProfile/UserProfile';
