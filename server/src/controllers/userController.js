4aad5d2f09f789a463f0921d9d06967a9e73f0c7
Kyrylo <fresh.js.ftd.@gmail.com>
2023-02-08 20:39:40 +0200

    Remove unused methods

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -6,9 +6,7 @@ const {
   Rating,
   Offer,
   Contest,
-  User,
 } = require('../models');
-const NotUniqueEmail = require('../errors/NotUniqueEmail');
 const moment = require('moment');
 const { v4: uuid } = require('uuid');
 const controller = require('../socketInit');
@@ -16,59 +14,6 @@ const userQueries = require('./queries/userQueries');
 const bankQueries = require('./queries/bankQueries');
 const ratingQueries = require('./queries/ratingQueries');
 
-module.exports.login = async (req, res, next) => {
-  try {
-    const foundUser = await User.findOne({ email: req.body.email });
-    await foundUser.comparePassword(req.body.password);
-    const accessToken = jwt.sign(
-      {
-        firstName: foundUser.firstName,
-        userId: foundUser.id,
-        role: foundUser.role,
-        lastName: foundUser.lastName,
-        avatar: foundUser.avatar,
-        displayName: foundUser.displayName,
-        balance: foundUser.balance,
-        email: foundUser.email,
-        rating: foundUser.rating,
-      },
-      CONSTANTS.JWT_SECRET,
-      { expiresIn: CONSTANTS.ACCESS_TOKEN_TIME }
-    );
-    await userQueries.updateUser({ accessToken }, foundUser.id);
-    res.send({ token: accessToken });
-  } catch (err) {
-    next(err);
-  }
-};
-module.exports.registration = async (req, res, next) => {
-  try {
-    const newUser = await userQueries.userCreation(req.body);
-    const accessToken = jwt.sign(
-      {
-        firstName: newUser.firstName,
-        userId: newUser.id,
-        role: newUser.role,
-        lastName: newUser.lastName,
-        avatar: newUser.avatar,
-        displayName: newUser.displayName,
-        balance: newUser.balance,
-        email: newUser.email,
-        rating: newUser.rating,
-      },
-      CONSTANTS.JWT_SECRET,
-      { expiresIn: CONSTANTS.ACCESS_TOKEN_TIME }
-    );
-    await userQueries.updateUser({ accessToken }, newUser.id);
-    res.send({ token: accessToken });
-  } catch (err) {
-    if (err.name === 'SequelizeUniqueConstraintError') {
-      next(new NotUniqueEmail());
-    } else {
-      next(err);
-    }
-  }
-};
 
 function getQuery(offerId, userId, mark, isFirst, transaction) {
   const getCreateQuery = () =>
