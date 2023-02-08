a40e34c84508b017eb4ac8e02259cfc76e9f5851
Kyrylo <fresh.js.ftd.@gmail.com>
2023-02-08 20:38:19 +0200

    Add authController

---
+++ server/src/controllers/authController.js
@@ -0,0 +1,48 @@
+const AuthService = require('../services/authService');
+const { User } = require('../models');
+
+module.exports.login = async (req, res, next) => {
+  try {
+    const {
+      body: { email, password },
+    } = req;
+
+    const user = await User.findOne({ where: { email } });
+
+    if (!user || !user.comparePassword(password)) {
+      return next(new UserNotFoundError('Invalid login data'));
+    }
+
+    const responseData = await AuthService.createSession(user);
+
+    res.send(responseData);
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports.registration = async (req, res, next) => {
+  try {
+    const { body } = req;
+
+    const user = await User.create(body);
+
+    const responseData = await AuthService.createSession(user);
+
+    res.status(201).send(responseData);
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports.refresh = async (req, res, next) => {
+  try {
+    const { refreshTokenInstance } = req;
+
+    const responseData = await AuthService.refreshSession(refreshTokenInstance);
+
+    res.send(responseData);
+  } catch (error) {
+    next(error);
+  }
+};
