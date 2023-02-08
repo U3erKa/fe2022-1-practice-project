dc50d0bacacc33f27bdb732f78aef8f71f1ee90a
Kyrylo <fresh.js.ftd.@gmail.com>
2023-02-08 20:35:58 +0200

    Refactor data in tokens

--- server/src/services/authService.js
+++ server/src/services/authService.js
@@ -3,9 +3,15 @@ const { RefreshToken } = require('../models');
 
 module.exports.createSession = async (user) => {
   const tokenPair = await JWTService.generateTokenPair({
-    id: user.id,
-    email: user.email,
+    firstName: user.firstName,
+    userId: user.id,
     role: user.role,
+    lastName: user.lastName,
+    avatar: user.avatar,
+    displayName: user.displayName,
+    balance: user.balance,
+    email: user.email,
+    rating: user.rating,
   });
 
   await RefreshToken.create({
@@ -20,9 +26,15 @@ module.exports.refreshSession = async (refreshTokenInstance) => {
   const user = await refreshTokenInstance.getUser();
 
   const tokenPair = await JWTService.generateTokenPair({
-    id: user.id,
-    email: user.email,
+    firstName: user.firstName,
+    userId: user.id,
     role: user.role,
+    lastName: user.lastName,
+    avatar: user.avatar,
+    displayName: user.displayName,
+    balance: user.balance,
+    email: user.email,
+    rating: user.rating,
   });
 
   await refreshTokenInstance.update({ token: tokenPair.refreshToken });
