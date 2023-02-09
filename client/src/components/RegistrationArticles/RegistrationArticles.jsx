826fd4adbd706bce97a247416472a3b3649faf45
Viktor Stepanov <vitya.zap@gmail.com>
2023-02-09 16:35:29 +0200

    Refactor RegistrationPage
    Move articles to separate component

---
+++ client/src/components/RegistrationArticles/RegistrationArticles.jsx
@@ -0,0 +1,32 @@
+import { Fragment } from 'react';
+
+import { TextEntry } from 'components';
+import styles from './RegistrationArticles.module.sass';
+
+/**
+ * @typedef {object} Article
+ * @property {string | import('components/TextEntry/TextEntry').TextEntryList} article
+ * @property {number} id
+ * @property {string} header
+ */
+/** @type {import('react').FC<{articles: Article[]}>} */
+const RegistrationArticles = ({ articles }) => {
+  return (
+    <div className={styles.ColumnContainer}>
+      {articles.map(({ id, header, article }) => (
+        <Fragment key={id}>
+          <div className={styles.headerArticle}>{header}</div>
+          {Array.isArray(article) ? (
+            <div className={styles.article}>
+              <TextEntry text={article} />
+            </div>
+          ) : (
+            <div className={styles.article}>{article}</div>
+          )}
+        </Fragment>
+      ))}
+    </div>
+  );
+};
+
+export default RegistrationArticles;
