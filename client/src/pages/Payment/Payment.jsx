ac20065378bf60aa4e95f747d0b684032b31b021
Viktor Stepanov <vitya.zap@gmail.com>
2023-02-11 15:53:13 +0200

    Refactor DialogList
    Replace connect with hooks

--- client/src/pages/Payment/Payment.jsx
+++ client/src/pages/Payment/Payment.jsx
@@ -1,4 +1,4 @@
-import { connect, useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import isEmpty from 'lodash/isEmpty';
 
