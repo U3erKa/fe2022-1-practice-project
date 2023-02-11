ce181ad17509ba94a33d4d9a8aa2d8407fbdd48c
Viktor Stepanov <vitya.zap@gmail.com>
2023-02-11 23:10:47 +0200

    Fix redirect to homepage on refresh

--- client/src/hocs/Private/Private.jsx
+++ client/src/hocs/Private/Private.jsx
@@ -1,19 +1,13 @@
-import { useSelector } from 'react-redux';
-
 import { Spinner } from 'components';
 
 const PrivateHoc = (Component, props) => {
   const Hoc = ({ isFetching, history, match }) => {
-    const user = useSelector((state) => state.userStore.data);
-
     return (
       <>
         {isFetching ? (
           <Spinner />
-        ) : user ? (
-          <Component history={history} match={match} {...props} />
         ) : (
-          history.replace('/login')
+          <Component history={history} match={match} {...props} />
         )}
       </>
     );
