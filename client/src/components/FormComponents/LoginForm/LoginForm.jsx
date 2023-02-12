583e3a42431f3d606c19c214d4d100fc3a29f82f
Viktor Stepanov <vitya.zap@gmail.com>
2023-02-12 18:32:16 +0200

    Convert auth components into FC

--- client/src/components/FormComponents/LoginForm/LoginForm.jsx
+++ client/src/components/FormComponents/LoginForm/LoginForm.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { Field, Form, Formik } from 'formik';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { Form, Formik } from 'formik';
 
 import { checkAuth, clearAuth } from 'store/slices/authSlice';
 import { Error } from 'components';
@@ -8,88 +9,76 @@ import { FormInput } from 'components/InputComponents';
 
 import { LoginSchem } from 'utils/validators/validationSchems';
 import { AUTH_MODE } from 'constants/general';
-import { withRouter } from 'hocs';
 import styles from './LoginForm.module.sass';
 
-class LoginForm extends React.Component {
-  componentWillUnmount() {
-    this.props.authClear();
-  }
+const LoginForm = () => {
+  const { error, isFetching } = useSelector(({ auth }) => auth);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-  clicked = (values) => {
-    this.props.loginRequest({ data: values, navigate: this.props.navigate });
+  useEffect(() => {
+    return () => {
+      dispatch(clearAuth());
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const onSubmit = (values) => {
+    dispatch(checkAuth({ data: values, navigate, authMode: AUTH_MODE.LOGIN }));
   };
 
-  render() {
-    const { error, isFetching } = this.props.auth;
-    const { submitting, authClear } = this.props;
+  const formInputClasses = {
+    container: styles.inputContainer,
+    input: styles.input,
+    warning: styles.fieldWarning,
+    notValid: styles.notValid,
+    valid: styles.valid,
+  };
 
-    const formInputClasses = {
-      container: styles.inputContainer,
-      input: styles.input,
-      warning: styles.fieldWarning,
-      notValid: styles.notValid,
-      valid: styles.valid,
-    };
-
-    return (
-      <div className={styles.loginForm}>
-        {error && (
-          <Error
-            data={error.data}
-            status={error.status}
-            clearError={authClear}
+  return (
+    <div className={styles.loginForm}>
+      {error && (
+        <Error
+          data={error.data}
+          status={error.status}
+          clearError={() => dispatch(clearAuth())}
+        />
+      )}
+      <h2>LOGIN TO YOUR ACCOUNT</h2>
+      <Formik
+        initialValues={{
+          email: '',
+          password: '',
+        }}
+        onSubmit={onSubmit}
+        validationSchema={LoginSchem}
+      >
+        <Form>
+          <FormInput
+            classes={formInputClasses}
+            name="email"
+            type="text"
+            label="Email Address"
           />
-        )}
-        <h2>LOGIN TO YOUR ACCOUNT</h2>
-        <Formik
-          initialValues={{
-            email: '',
-            password: '',
-          }}
-          onSubmit={this.clicked}
-          validationSchema={LoginSchem}
-        >
-          <Form>
-            <FormInput
-              classes={formInputClasses}
-              name="email"
-              type="text"
-              label="Email Address"
-            />
-            <FormInput
-              classes={formInputClasses}
-              name="password"
-              type="password"
-              label="Password"
-            />
-            <button
-              type="submit"
-              disabled={submitting}
-              className={styles.submitContainer}
-            >
-              <span className={styles.inscription}>
-                {isFetching ? 'Submitting...' : 'LOGIN'}
-              </span>
-            </button>
-          </Form>
-        </Formik>
-      </div>
-    );
-  }
-}
-
-const mapStateToProps = (state) => {
-  const { auth } = state;
-  return { auth };
+          <FormInput
+            classes={formInputClasses}
+            name="password"
+            type="password"
+            label="Password"
+          />
+          <button
+            type="submit"
+            disabled={isFetching}
+            className={styles.submitContainer}
+          >
+            <span className={styles.inscription}>
+              {isFetching ? 'Submitting...' : 'Login'}
+            </span>
+          </button>
+        </Form>
+      </Formik>
+    </div>
+  );
 };
 
-const mapDispatchToProps = (dispatch) => ({
-  loginRequest: ({ data, navigate }) =>
-    dispatch(checkAuth({ data, navigate, authMode: AUTH_MODE.LOGIN })),
-  authClear: () => dispatch(clearAuth()),
-});
-
-export default withRouter(
-  connect(mapStateToProps, mapDispatchToProps)(LoginForm),
-);
+export default LoginForm;
--- client/src/components/FormComponents/RegistrationForm/RegistrationForm.jsx
+++ client/src/components/FormComponents/RegistrationForm/RegistrationForm.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import { Field, Form, Formik } from 'formik';
 
 import { checkAuth, clearAuth } from 'store/slices/authSlice';
@@ -12,166 +13,147 @@ import {
 
 import { RegistrationSchem } from 'utils/validators/validationSchems';
 import { AUTH_MODE, CUSTOMER, CREATOR } from 'constants/general';
-import { withRouter } from 'hocs';
 import styles from './RegistrationForm.module.sass';
 
-class RegistrationForm extends React.Component {
-  componentWillUnmount() {
-    this.props.authClear();
-  }
+const RegistrationForm = () => {
+  const { error, isFetching } = useSelector(({ auth }) => auth);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-  clicked = (values) => {
-    this.props.register({
-      data: {
-        firstName: values.firstName,
-        lastName: values.lastName,
-        displayName: values.displayName,
-        email: values.email,
-        password: values.password,
-        role: values.role,
-      },
-      navigate: this.props.navigate,
-    });
+  useEffect(() => {
+    return () => {
+      dispatch(clearAuth());
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const onSubmit = (values) => {
+    dispatch(
+      checkAuth({ data: values, navigate, authMode: AUTH_MODE.REGISTER }),
+    );
   };
 
-  render() {
-    const { submitting, auth, authClear } = this.props;
-    const { error } = auth;
-    const formInputClasses = {
-      container: styles.inputContainer,
-      input: styles.input,
-      warning: styles.fieldWarning,
-      notValid: styles.notValid,
-      valid: styles.valid,
-    };
-    return (
-      <div className={styles.signUpFormContainer}>
-        {error && (
-          <Error
-            data={error.data}
-            status={error.status}
-            clearError={authClear}
-          />
-        )}
-        <div className={styles.headerFormContainer}>
-          <h2>CREATE AN ACCOUNT</h2>
-          <h4>We always keep your name and email address private.</h4>
-        </div>
-        <Formik
-          initialValues={{
-            firstName: '',
-            lastName: '',
-            displayName: '',
-            email: '',
-            password: '',
-            confirmPassword: '',
-            role: CUSTOMER,
-            agreeOfTerms: false,
-          }}
-          onSubmit={this.clicked}
-          validationSchema={RegistrationSchem}
-        >
-          <Form>
-            <div className={styles.row}>
-              <FormInput
-                name="firstName"
-                classes={formInputClasses}
-                type="text"
-                label="First name"
-              />
-              <FormInput
-                name="lastName"
-                classes={formInputClasses}
-                type="text"
-                label="Last name"
-              />
-            </div>
-            <div className={styles.row}>
-              <FormInput
-                name="displayName"
-                classes={formInputClasses}
-                type="text"
-                label="Display Name"
-              />
-              <FormInput
-                name="email"
-                classes={formInputClasses}
-                type="text"
-                label="Email Address"
-              />
-            </div>
-            <div className={styles.row}>
-              <FormInput
-                name="password"
-                classes={formInputClasses}
-                type="password"
-                label="Password"
-              />
-              <FormInput
-                name="confirmPassword"
-                classes={formInputClasses}
-                type="password"
-                label="Password confirmation"
-              />
-            </div>
-            <div className={styles.choseRoleContainer}>
-              <Field
-                name="role"
-                type="radio"
-                value={CUSTOMER}
-                strRole="Join As a Buyer"
-                infoRole="I am looking for a Name, Logo or Tagline for my business, brand or product."
-                component={RoleInput}
-                id={CUSTOMER}
-              />
-              <Field
-                name="role"
-                type="radio"
-                value={CREATOR}
-                strRole="Join As a Creative"
-                infoRole="I plan to submit name ideas, Logo designs or sell names in Domain Marketplace."
-                component={RoleInput}
-                id={CREATOR}
-              />
-            </div>
-            <div className={styles.termsOfService}>
-              <AgreeTermOfServiceInput
-                name="agreeOfTerms"
-                classes={{
-                  container: styles.termsOfService,
-                  warning: styles.fieldWarning,
-                }}
-                id="termsOfService"
-                type="checkbox"
-              />
-            </div>
-            <button
-              type="submit"
-              disabled={submitting}
-              className={styles.submitContainer}
-            >
-              <span className={styles.inscription}>Create Account</span>
-            </button>
-          </Form>
-        </Formik>
+  const formInputClasses = {
+    container: styles.inputContainer,
+    input: styles.input,
+    warning: styles.fieldWarning,
+    notValid: styles.notValid,
+    valid: styles.valid,
+  };
+  return (
+    <div className={styles.signUpFormContainer}>
+      {error && (
+        <Error
+          data={error.data}
+          status={error.status}
+          clearError={() => dispatch(clearAuth())}
+        />
+      )}
+      <div className={styles.headerFormContainer}>
+        <h2>CREATE AN ACCOUNT</h2>
+        <h4>We always keep your name and email address private.</h4>
       </div>
-    );
-  }
-}
+      <Formik
+        initialValues={{
+          firstName: '',
+          lastName: '',
+          displayName: '',
+          email: '',
+          password: '',
+          confirmPassword: '',
+          role: CUSTOMER,
+          agreeOfTerms: false,
+        }}
+        onSubmit={onSubmit}
+        validationSchema={RegistrationSchem}
+      >
+        <Form>
+          <div className={styles.row}>
+            <FormInput
+              name="firstName"
+              classes={formInputClasses}
+              type="text"
+              label="First name"
+            />
+            <FormInput
+              name="lastName"
+              classes={formInputClasses}
+              type="text"
+              label="Last name"
+            />
+          </div>
+          <div className={styles.row}>
+            <FormInput
+              name="displayName"
+              classes={formInputClasses}
+              type="text"
+              label="Display Name"
+            />
+            <FormInput
+              name="email"
+              classes={formInputClasses}
+              type="text"
+              label="Email Address"
+            />
+          </div>
+          <div className={styles.row}>
+            <FormInput
+              name="password"
+              classes={formInputClasses}
+              type="password"
+              label="Password"
+            />
+            <FormInput
+              name="confirmPassword"
+              classes={formInputClasses}
+              type="password"
+              label="Password confirmation"
+            />
+          </div>
+          <div className={styles.choseRoleContainer}>
+            <Field
+              name="role"
+              type="radio"
+              value={CUSTOMER}
+              strRole="Join As a Buyer"
+              infoRole="I am looking for a Name, Logo or Tagline for my business, brand or product."
+              component={RoleInput}
+              id={CUSTOMER}
+            />
+            <Field
+              name="role"
+              type="radio"
+              value={CREATOR}
+              strRole="Join As a Creative"
+              infoRole="I plan to submit name ideas, Logo designs or sell names in Domain Marketplace."
+              component={RoleInput}
+              id={CREATOR}
+            />
+          </div>
+          <div className={styles.termsOfService}>
+            <AgreeTermOfServiceInput
+              name="agreeOfTerms"
+              classes={{
+                container: styles.termsOfService,
+                warning: styles.fieldWarning,
+              }}
+              id="termsOfService"
+              type="checkbox"
+            />
+          </div>
+          <button
+            type="submit"
+            disabled={isFetching}
+            className={styles.submitContainer}
+          >
+            <span className={styles.inscription}>
+              {isFetching ? 'Submitting...' : 'Create Account'}
+            </span>
+          </button>
+        </Form>
+      </Formik>
+    </div>
+  );
+};
 
-const mapStateToProps = (state) => ({
-  auth: state.auth,
-  initialValues: {
-    role: CUSTOMER,
-  },
-});
-
-const mapDispatchToProps = (dispatch) => ({
-  register: ({ data, navigate }) =>
-    dispatch(checkAuth({ data, navigate, authMode: AUTH_MODE.REGISTER })),
-  authClear: () => dispatch(clearAuth()),
-});
-
-export default withRouter(
-  connect(mapStateToProps, mapDispatchToProps)(RegistrationForm),
-);
+export default RegistrationForm;
