e7aaa18387edce472475ca0ec48b08f7a4946c60
Viktor Stepanov <vitya.zap@gmail.com>
2023-02-13 21:25:25 +0200

    Refactor UserInfo
    Replace last connect with hooks

--- client/src/components/UserInfo/UserInfo.jsx
+++ client/src/components/UserInfo/UserInfo.jsx
@@ -1,4 +1,4 @@
-import { connect } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { updateUser } from 'store/slices/userSlice';
 import { changeEditModeOnUserProfile } from 'store/slices/userProfileSlice';
@@ -7,19 +7,62 @@ import { UpdateUserInfoForm } from 'components/FormComponents';
 import { ANONYM_IMAGE_PATH, PUBLIC_URL, CREATOR } from 'constants/general';
 import styles from './UserInfo.module.sass';
 
-const UserInfo = (props) => {
+const UserInfoData = ({ userData }) => {
+  const { firstName, lastName, displayName, email, role, balance } = userData;
+
+  return (
+    <div className={styles.infoContainer}>
+      <div className={styles.infoBlock}>
+        <span className={styles.label}>First Name</span>
+        <span className={styles.info}>{firstName}</span>
+      </div>
+      <div className={styles.infoBlock}>
+        <span className={styles.label}>Last Name</span>
+        <span className={styles.info}>{lastName}</span>
+      </div>
+      <div className={styles.infoBlock}>
+        <span className={styles.label}>Display Name</span>
+        <span className={styles.info}>{displayName}</span>
+      </div>
+      <div className={styles.infoBlock}>
+        <span className={styles.label}>Email</span>
+        <span className={styles.info}>{email}</span>
+      </div>
+      <div className={styles.infoBlock}>
+        <span className={styles.label}>Role</span>
+        <span className={styles.info}>{role}</span>
+      </div>
+      {role === CREATOR && (
+        <div className={styles.infoBlock}>
+          <span className={styles.label}>Balance</span>
+          <span className={styles.info}>{`${balance}$`}</span>
+        </div>
+      )}
+    </div>
+  );
+};
+
+const UserInfo = () => {
+  const { data, isEdit } = useSelector((state) => {
+    const { data } = state.userStore;
+    const { isEdit } = state.userProfile;
+    return { data, isEdit };
+  });
+  const dispatch = useDispatch();
+
+  const { avatar, ...userData } = data || {};
+
   const updateUserData = (values) => {
     const formData = new FormData();
+
     formData.append('file', values.file);
     formData.append('firstName', values.firstName);
     formData.append('lastName', values.lastName);
     formData.append('displayName', values.displayName);
-    props.updateUser(formData);
+
+    dispatch(updateUser(formData));
   };
 
-  const { isEdit, changeEditMode, data } = props;
-  const { avatar, firstName, lastName, displayName, email, role, balance } =
-    data || {};
   return (
     <div className={styles.mainContainer}>
       {isEdit ? (
@@ -35,38 +78,11 @@ const UserInfo = (props) => {
             className={styles.avatar}
             alt="user"
           />
-          <div className={styles.infoContainer}>
-            <div className={styles.infoBlock}>
-              <span className={styles.label}>First Name</span>
-              <span className={styles.info}>{firstName}</span>
-            </div>
-            <div className={styles.infoBlock}>
-              <span className={styles.label}>Last Name</span>
-              <span className={styles.info}>{lastName}</span>
-            </div>
-            <div className={styles.infoBlock}>
-              <span className={styles.label}>Display Name</span>
-              <span className={styles.info}>{displayName}</span>
-            </div>
-            <div className={styles.infoBlock}>
-              <span className={styles.label}>Email</span>
-              <span className={styles.info}>{email}</span>
-            </div>
-            <div className={styles.infoBlock}>
-              <span className={styles.label}>Role</span>
-              <span className={styles.info}>{role}</span>
-            </div>
-            {role === CREATOR && (
-              <div className={styles.infoBlock}>
-                <span className={styles.label}>Balance</span>
-                <span className={styles.info}>{`${balance}$`}</span>
-              </div>
-            )}
-          </div>
+          <UserInfoData userData={userData} />
         </div>
       )}
       <div
-        onClick={() => changeEditMode(!isEdit)}
+        onClick={() => dispatch(changeEditModeOnUserProfile(!isEdit))}
         className={styles.buttonEdit}
       >
         {isEdit ? 'Cancel' : 'Edit'}
@@ -75,15 +91,4 @@ const UserInfo = (props) => {
   );
 };
 
-const mapStateToProps = (state) => {
-  const { data } = state.userStore;
-  const { isEdit } = state.userProfile;
-  return { data, isEdit };
-};
-
-const mapDispatchToProps = (dispatch) => ({
-  updateUser: (data) => dispatch(updateUser(data)),
-  changeEditMode: (data) => dispatch(changeEditModeOnUserProfile(data)),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(UserInfo);
+export default UserInfo;
