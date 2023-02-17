056169e6bb03d3edb624906aa482c353420d77ff
Viktor Stepanov <vitya.zap@gmail.com>
2023-02-17 19:32:52 +0200

    Fix spinner position in ContestsContainer

--- client/src/components/contest/ContestsContainer.jsx
+++ client/src/components/contest/ContestsContainer.jsx
@@ -1,15 +1,19 @@
 import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 import { Spinner } from 'components/general';
-import styles from './styles/ContestContainer.module.sass';
-import { useNavigate } from 'react-router-dom';
 import ContestBox from './ContestBox';
+import styles from './styles/ContestContainer.module.sass';
 
 const ContestsContainer = ({ loadMore }) => {
   const { isFetching, haveMore, contests } = useSelector(
     (state) => state.contestsList,
   );
   const navigate = useNavigate();
+
+  const contestsList = contests.map((contest) => (
+    <ContestBox data={contest} key={contest.id} goToExtended={goToExtended} />
+  ));
 
   const goToExtended = (contest_id) => {
     navigate(`/contest/${contest_id}`);
@@ -28,30 +32,21 @@ const ContestsContainer = ({ loadMore }) => {
     }
   };
 
-  if (isFetching) {
-    return (
-      <div className={styles.spinnerContainer}>
-        <Spinner />
-      </div>
-    );
-  }
-
-  if (contests.length === 0) {
-    return <div className={styles.notFound}>Nothing was found</div>;
-  }
-
   return (
     <div>
-      {contests.map((contest) => (
-        <ContestBox
-          data={contest}
-          key={contest.id}
-          goToExtended={goToExtended}
-        />
-      ))}
-      <button onClick={onClick} disabled={!haveMore}>
-        Load more
-      </button>
+      {contestsList}
+      {isFetching && (
+        <div className={styles.spinnerContainer}>
+          <Spinner />
+        </div>
+      )}
+      {contests.length ? (
+        <button onClick={onClick} disabled={!contests.length || !haveMore}>
+          Load more
+        </button>
+      ) : (
+        <div className={styles.notFound}>Nothing was found</div>
+      )}
     </div>
   );
 };
