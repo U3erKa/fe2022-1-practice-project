c82d2d5f3ba4083b2254754ebf78b10e8a374f0e
Viktor Stepanov <vitya.zap@gmail.com>
2023-02-20 05:29:13 +0200

    Refactor API types

--- client/src/types/api/user.ts
+++ client/src/types/api/user.ts
@@ -1,4 +1,4 @@
-import type { UserId, WithId } from './_common';
+import type { JWT, UserId, WithId } from './_common';
 import type { CREATOR, CUSTOMER } from 'constants/general';
 
 export type UpdateUserResponse = UserWithoutPassword;
@@ -12,11 +12,19 @@ export type User = WithId<UserId> & {
   firstName: string;
   lastName: string;
   displayName: string;
-  password: string;
-  email: string;
   avatar: string;
+  email: string;
+  password: string;
   role: typeof CUSTOMER | typeof CREATOR;
-  balance: string | number;
-  accessToken: string;
+  balance: Card['balance'];
+  accessToken: JWT;
   rating: number;
+};
+
+export type Card = {
+  name: string;
+  number: string | number;
+  expiry: string | number;
+  cvc: string | number;
+  balance: string | number;
 };
