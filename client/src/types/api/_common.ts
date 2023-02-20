56f398db57e0f5bf8770744e7b30798d7162739d
Viktor Stepanov <vitya.zap@gmail.com>
2023-02-20 03:31:36 +0200

    Define offer types

--- client/src/types/api/_common.ts
+++ client/src/types/api/_common.ts
@@ -1,27 +1,39 @@
 export type With_id<T extends _Id = _Id> = { _id: T };
 export type With__v = { __v: __V };
-export type WithId<K extends string = 'id', T extends Id = Id> = {
+export type WithId<T extends Id = Id, K extends string = 'id'> = {
+  [key in K]: T;
+};
+export type WithUUID<T extends UUID = UUID, K extends string = 'id'> = {
   [key in K]: T;
 };
 
 export type WithCatalogId = { catalogId: CatalogId };
 export type WithChatId = { chatId: ChatId };
 export type WithTimeStamps = { createdAt: TimeStamp; updatedAt: TimeStamp };
+export type WithLifeSpan = { iat: NumTimeStamp; exp: NumTimeStamp };
 
-/** Unique integer */
+/** SQL's unique id parameter */
 export type Id = number;
 /** MongoDB's `_id` parameter */
 export type _Id = string;
 /** MongoDB's `__v` parameter */
 export type __V = 0;
+export type UUID = string;
 
 export type CatalogId = _Id;
 export type CatalogListId = _Id;
 export type ChatId = _Id;
 export type ConversationId = _Id;
 export type MessageId = _Id;
+
+export type ContestId = Id;
+export type CreatorId = Id;
+export type OfferId = Id;
 export type InterlocutorId = Id;
 export type SenderId = Id;
 export type UserId = Id;
 
+export type OrderId = UUID;
+
 export type TimeStamp = string;
+export type NumTimeStamp = number;
