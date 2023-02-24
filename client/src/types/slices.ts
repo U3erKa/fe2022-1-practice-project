735a381605e7b5bc893c38149461245ab11ecc4f
Viktor Stepanov <vitya.zap@gmail.com>
2023-02-24 14:24:24 +0200

    Define bundliSlice types

--- client/src/types/slices.ts
+++ client/src/types/slices.ts
@@ -4,3 +4,15 @@ export type AuthState = {
   error: Error | null;
 };
 
+export type BundleState = {
+  bundle: Bundle | null;
+};
+
+export type Bundle = {
+  first: Omit<ContestsOrder, 'payment'>;
+  name: Omit<ContestsOrder, 'name'>;
+  logo: Omit<ContestsOrder, 'name' | 'logo'>;
+  tagline: Omit<ContestsOrder, 'name' | 'logo' | 'payment'>;
+};
+
+export type ContestsOrder = 'name' | 'logo' | 'tagline' | 'payment';
