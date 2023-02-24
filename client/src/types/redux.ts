6ef8ebdb1223053fcd15430b458329290fd06545
Viktor Stepanov <vitya.zap@gmail.com>
2023-02-24 18:25:25 +0200

    Fix decorateAsyncThunk type

--- client/src/types/redux.ts
+++ client/src/types/redux.ts
@@ -1,11 +1,15 @@
-import type { ActionReducerMapBuilder, AsyncThunk } from '@reduxjs/toolkit';
+import type {
+  ActionReducerMapBuilder,
+  AsyncThunk,
+  AsyncThunkPayloadCreator,
+} from '@reduxjs/toolkit';
 
-export type AsyncThunkDecorator = <Return, Payload = void>({
+export type AsyncThunkDecorator = <Return, Payload = void, Arg = void>({
   key,
   thunk,
 }: {
   key: string;
-  thunk: (payload: Payload, thunkAPI?: unknown) => Return;
+  thunk: AsyncThunkPayloadCreator<Return, Arg>;
 }) => AsyncThunk<Return, Payload, {}>;
 
 export type ExtraReducersCreator = <State, Return, Payload>(options: {
