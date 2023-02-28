7eca12e911d5ae30eed890e7d32c63e27cf3bef8
Viktor Stepanov <vitya.zap@gmail.com>
2023-02-28 20:44:02 +0200

    Fix filterSchem

--- client/src/utils/validators/validationSchems.ts
+++ client/src/utils/validators/validationSchems.ts
@@ -100,7 +100,7 @@ export const ContestSchem = yup.object({
 });
 
 export const filterSchem = yup.object().shape({
-  typeIndex: yup.number().oneOf[(1, 2, 3, 4, 5, 6, 7)],
+  typeIndex: yup.number().oneOf([1, 2, 3, 4, 5, 6, 7]),
   contestId: yup.string(),
   awardSort: yup.string().matches(/(desc|asc)/),
   industry: yup.string(),
