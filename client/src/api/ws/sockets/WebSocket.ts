9602b439d183b1dc6be48def44f17eb677ead736
Viktor Stepanov <vitya.zap@gmail.com>
2023-03-01 01:09:01 +0200

    Infer WebSocket types

--- client/src/api/ws/sockets/WebSocket.ts
+++ client/src/api/ws/sockets/WebSocket.ts
@@ -1,12 +1,17 @@
 import socketIoClient from 'socket.io-client';
 
 import { BASE_URL } from 'constants/general';
+import type { AppDispatch, RootState } from 'store';
 
 class WebSocket {
-  constructor(dispatch, getState, room) {
-    this.dispatch = dispatch;
-    this.getState = getState;
+  socket: ReturnType<typeof socketIoClient>;
+  constructor(
+    public dispatch: AppDispatch,
+    public getState: RootState,
+    room: string,
+  ) {
     this.socket = socketIoClient(`${BASE_URL}${room}`, {
+      // @ts-expect-error
       origins: 'localhost:*',
     });
     this.listen();
