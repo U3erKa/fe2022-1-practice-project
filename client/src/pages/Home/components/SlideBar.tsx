1bf88a3f00c624783519a16b79c0eae5b062126d
Viktor Stepanov <vitya.zap@gmail.com>
2023-02-28 14:50:08 +0200

    Infer SlideBar props

--- client/src/pages/Home/components/SlideBar.tsx
+++ client/src/pages/Home/components/SlideBar.tsx
@@ -11,7 +11,17 @@ import {
 import style from '../styles/SlideBar.module.sass';
 import '../styles/flickity.css';
 
-const SliderBar = (props) => {
+import type { FC } from 'react';
+
+export type Props = {
+  carouselType:
+    | typeof MAIN_SLIDER
+    | typeof EXAMPLE_SLIDER
+    | typeof FEEDBACK_SLIDER;
+  images: string[];
+};
+
+const SliderBar: FC<Props> = ({ carouselType, images }) => {
   const options = {
     draggable: true,
     wrapAround: true,
@@ -23,7 +33,6 @@ const SliderBar = (props) => {
   };
 
   const getStyleName = () => {
-    const { carouselType } = props;
     switch (carouselType) {
       case MAIN_SLIDER:
         return style.mainCarousel;
@@ -38,12 +47,11 @@ const SliderBar = (props) => {
   };
 
   const renderSlides = () => {
-    const { carouselType } = props;
     switch (carouselType) {
       case MAIN_SLIDER: {
-        return Object.keys(props.images).map((key, index) => (
+        return images.map((image, index) => (
           <img
-            src={props.images[key]}
+            src={image}
             alt="slide"
             key={index}
             className={style['carousel-cell']}
@@ -51,17 +59,17 @@ const SliderBar = (props) => {
         ));
       }
       case EXAMPLE_SLIDER: {
-        return Object.keys(props.images).map((key, index) => (
+        return images.map((image, index) => (
           <div className={style['example-cell']} key={index}>
-            <img src={props.images[key]} alt="slide" />
+            <img src={image} alt="slide" />
             <p>{EXAMPLE_SLIDER_TEXT[index]}</p>
           </div>
         ));
       }
       case FEEDBACK_SLIDER: {
-        return Object.keys(props.images).map((key, index) => (
+        return images.map((image, index) => (
           <div className={style['feedback-cell']} key={index}>
-            <img src={props.images[key]} alt="slide" />
+            <img src={image} alt="slide" />
             <p>{FEEDBACK_SLIDER_TEXT[index].feedback}</p>
             <span>{FEEDBACK_SLIDER_TEXT[index].name}</span>
           </div>
