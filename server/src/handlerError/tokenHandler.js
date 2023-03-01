9220f14fdbfe9a6896213f493707246117d03cec
Viktor Stepanov <vitya.zap@gmail.com>
2023-03-01 21:47:41 +0200

    Fix access token not being refreshed on expire

--- server/src/handlerError/tokenHandler.js
+++ server/src/handlerError/tokenHandler.js
@@ -3,11 +3,11 @@ const TokenError = require('../errors/TokenError');
 const TokenExpirationError = require('../errors/TokenExpirationError');
 
 module.exports = async (err, req, res, next) => {
-  if (err instanceof JsonWebTokenError) {
-    return next(new TokenError());
-  }
   if (err instanceof TokenExpiredError) {
     return next(new TokenExpirationError());
+  }
+  if (err instanceof JsonWebTokenError) {
+    return next(new TokenError());
   }
 
   next(err);
