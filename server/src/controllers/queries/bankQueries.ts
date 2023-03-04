31f5ad223a0dc13ed03ceb8aedd19424125aec7c
Viktor Stepanov <vitya.zap@gmail.com>
2023-03-04 03:29:38 +0200

    Make transaction type optional

--- server/src/controllers/queries/bankQueries.ts
+++ server/src/controllers/queries/bankQueries.ts
@@ -1,7 +1,7 @@
 import { Bank } from '../../models';
 import BankDeclineError from '../../errors/BankDeclineError';
 
-export const updateBankBalance = async (data, predicate, transaction) => {
+export const updateBankBalance = async (data, predicate, transaction?) => {
   const [updatedCount, [updatedBank]] = await Bank.update(data, {
     where: predicate,
     returning: true,
