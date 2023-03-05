55a94df27f0cb5aef9b9ca3faedfd76d0a45588c
Viktor Stepanov <vitya.zap@gmail.com>
2023-03-05 23:49:22 +0200

    Define Rating types for associations

--- server/src/models/rating.ts
+++ server/src/models/rating.ts
@@ -2,6 +2,8 @@ import { Model } from 'sequelize';
 // prettier-ignore
 import type { 
   DataTypes as _DataTypes, InferAttributes, InferCreationAttributes,
+  NonAttribute, ForeignKey, Association,
+  BelongsToCreateAssociationMixin, BelongsToGetAssociationMixin, BelongsToSetAssociationMixin,
 } from 'sequelize';
 import type { DB } from '../types/models';
 
@@ -10,15 +12,33 @@ const Rating = (sequelize: DB['sequelize'], DataTypes: typeof _DataTypes) => {
     InferAttributes<Rating>,
     InferCreationAttributes<Rating>
   > {
-    declare mark: number;
-
-    declare offerId: number;
-    declare userId: number;
-
     static associate({ User, Offer }: DB) {
       Rating.belongsTo(User, { foreignKey: 'userId', targetKey: 'id' });
       Rating.belongsTo(Offer, { foreignKey: 'offerId', targetKey: 'id' });
     }
+
+    //#region Model declarations
+    declare mark: number;
+
+    declare offerId: ForeignKey<InstanceType<DB['Offer']>['id']>;
+    declare userId: ForeignKey<InstanceType<DB['User']>['id']>;
+
+    declare user?: NonAttribute<DB['User'][]>;
+    declare offer?: NonAttribute<DB['Offer'][]>;
+
+    declare static associations: {
+      user: Association<DB['Rating'], DB['User']>;
+      offer: Association<DB['Rating'], DB['Offer']>;
+    };
+
+    declare createUser: BelongsToCreateAssociationMixin<DB['User']>;
+    declare getUser: BelongsToGetAssociationMixin<DB['User']>;
+    declare addUser: BelongsToSetAssociationMixin<DB['User'], number>;
+
+    declare createOffer: BelongsToCreateAssociationMixin<DB['Offer']>;
+    declare getOffer: BelongsToGetAssociationMixin<DB['Offer']>;
+    declare addOffer: BelongsToSetAssociationMixin<DB['Offer'], number>;
+    //#endregion
   }
   Rating.init(
     {
