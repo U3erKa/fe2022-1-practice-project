f566bdb28be1a779fddee170baa4a4cc6be261ea
Viktor Stepanov <vitya.zap@gmail.com>
2023-03-05 23:49:37 +0200

    Define User types for associations

--- server/src/models/user.ts
+++ server/src/models/user.ts
@@ -4,6 +4,12 @@ import { SALT_ROUNDS } from '../constants';
 // prettier-ignore
 import type { 
   DataTypes as _DataTypes, InferAttributes, InferCreationAttributes, CreationOptional,
+  NonAttribute, Association,
+  HasManyGetAssociationsMixin, HasManyAddAssociationMixin,
+  HasManyAddAssociationsMixin, HasManySetAssociationsMixin,
+  HasManyRemoveAssociationMixin, HasManyRemoveAssociationsMixin,
+  HasManyHasAssociationMixin, HasManyHasAssociationsMixin,
+  HasManyCountAssociationsMixin, HasManyCreateAssociationMixin,
 } from 'sequelize';
 import type { DB } from '../types/models';
 
@@ -19,6 +25,17 @@ const User = (sequelize: DB['sequelize'], DataTypes: typeof _DataTypes) => {
     InferAttributes<User>,
     InferCreationAttributes<User>
   > {
+    async comparePassword(password: string | Buffer) {
+      return bcrypt.compare(password, this.password);
+    }
+    static associate({ Offer, Contest, Rating, RefreshToken }: DB) {
+      User.hasMany(Offer, { foreignKey: 'userId', sourceKey: 'id' });
+      User.hasMany(Contest, { foreignKey: 'userId', sourceKey: 'id' });
+      User.hasMany(Rating, { foreignKey: 'userId', sourceKey: 'id' });
+      User.hasMany(RefreshToken, { foreignKey: 'userId', sourceKey: 'id' });
+    }
+
+    //#region Model declarations
     declare firstName: string;
     declare lastName: string;
     declare displayName: string;
@@ -32,15 +49,92 @@ const User = (sequelize: DB['sequelize'], DataTypes: typeof _DataTypes) => {
 
     declare id: CreationOptional<number>;
 
-    async comparePassword(password) {
-      return bcrypt.compare(password, this.password);
-    }
-    static associate({ Offer, Contest, Rating, RefreshToken }: DB) {
-      User.hasMany(Offer, { foreignKey: 'userId', sourceKey: 'id' });
-      User.hasMany(Contest, { foreignKey: 'userId', sourceKey: 'id' });
-      User.hasMany(Rating, { foreignKey: 'userId', sourceKey: 'id' });
-      User.hasMany(RefreshToken, { foreignKey: 'userId', sourceKey: 'id' });
-    }
+    declare offers?: NonAttribute<DB['Offer'][]>;
+    declare contests?: NonAttribute<DB['Contest'][]>;
+    declare ratings?: NonAttribute<DB['Rating'][]>;
+    declare refreshTokens?: NonAttribute<DB['RefreshToken'][]>;
+
+    declare static associations: {
+      offers: Association<DB['User'], DB['Offer']>;
+      contests: Association<DB['User'], DB['Contest']>;
+      ratings: Association<DB['User'], DB['Rating']>;
+      refreshTokens: Association<DB['User'], DB['RefreshToken']>;
+    };
+
+    declare getOffers: HasManyGetAssociationsMixin<DB['Offer']>;
+    declare addOffer: HasManyAddAssociationMixin<DB['Offer'], number>;
+    declare addOffers: HasManyAddAssociationsMixin<DB['Offer'], number>;
+    declare setOffers: HasManySetAssociationsMixin<DB['Offer'], number>;
+    declare removeOffer: HasManyRemoveAssociationMixin<DB['Offer'], number>;
+    declare removeOffers: HasManyRemoveAssociationsMixin<DB['Offer'], number>;
+    declare hasOffer: HasManyHasAssociationMixin<DB['Offer'], number>;
+    declare hasOffers: HasManyHasAssociationsMixin<DB['Offer'], number>;
+    declare countOffers: HasManyCountAssociationsMixin;
+    declare createOffer: HasManyCreateAssociationMixin<DB['Offer'], 'userId'>;
+
+    declare getContests: HasManyGetAssociationsMixin<DB['Contest']>;
+    declare addContest: HasManyAddAssociationMixin<DB['Contest'], number>;
+    declare addContests: HasManyAddAssociationsMixin<DB['Contest'], number>;
+    declare setContests: HasManySetAssociationsMixin<DB['Contest'], number>;
+    declare removeContest: HasManyRemoveAssociationMixin<DB['Contest'], number>;
+    declare removeContests: HasManyRemoveAssociationsMixin<
+      DB['Contest'],
+      number
+    >;
+    declare hasContest: HasManyHasAssociationMixin<DB['Contest'], number>;
+    declare hasContests: HasManyHasAssociationsMixin<DB['Contest'], number>;
+    declare countContests: HasManyCountAssociationsMixin;
+    declare createContest: HasManyCreateAssociationMixin<
+      DB['Contest'],
+      'userId'
+    >;
+
+    declare getRatings: HasManyGetAssociationsMixin<DB['Rating']>;
+    declare addRating: HasManyAddAssociationMixin<DB['Rating'], number>;
+    declare addRatings: HasManyAddAssociationsMixin<DB['Rating'], number>;
+    declare setRatings: HasManySetAssociationsMixin<DB['Rating'], number>;
+    declare removeRating: HasManyRemoveAssociationMixin<DB['Rating'], number>;
+    declare removeRatings: HasManyRemoveAssociationsMixin<DB['Rating'], number>;
+    declare hasRating: HasManyHasAssociationMixin<DB['Rating'], number>;
+    declare hasRatings: HasManyHasAssociationsMixin<DB['Rating'], number>;
+    declare countRatings: HasManyCountAssociationsMixin;
+    declare createRating: HasManyCreateAssociationMixin<DB['Rating'], 'userId'>;
+
+    declare getRefreshTokens: HasManyGetAssociationsMixin<DB['RefreshToken']>;
+    declare addRefreshToken: HasManyAddAssociationMixin<
+      DB['RefreshToken'],
+      number
+    >;
+    declare addRefreshTokens: HasManyAddAssociationsMixin<
+      DB['RefreshToken'],
+      number
+    >;
+    declare setRefreshTokens: HasManySetAssociationsMixin<
+      DB['RefreshToken'],
+      number
+    >;
+    declare removeRefreshToken: HasManyRemoveAssociationMixin<
+      DB['RefreshToken'],
+      number
+    >;
+    declare removeRefreshTokens: HasManyRemoveAssociationsMixin<
+      DB['RefreshToken'],
+      number
+    >;
+    declare hasRefreshToken: HasManyHasAssociationMixin<
+      DB['RefreshToken'],
+      number
+    >;
+    declare hasRefreshTokens: HasManyHasAssociationsMixin<
+      DB['RefreshToken'],
+      number
+    >;
+    declare countRefreshTokens: HasManyCountAssociationsMixin;
+    declare createRefreshToken: HasManyCreateAssociationMixin<
+      DB['RefreshToken'],
+      'userId'
+    >;
+    //#endregion
   }
   User.init(
     {
