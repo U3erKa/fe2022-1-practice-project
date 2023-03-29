b193d03f5225ad4804eb094d1beb8c29927011dd
Viktor Stepanov <vitya.zap@gmail.com>
2023-03-29 10:47:53 +0300

    Add styles for GetStarted

--- client/src/pages/HowItWorksPage/components/GetStarted.tsx
+++ client/src/pages/HowItWorksPage/components/GetStarted.tsx
@@ -2,7 +2,9 @@ import { Fragment } from 'react';
 import { Link } from 'react-router-dom';
 import { uniqueId } from 'lodash';
 import { GET_STARTED_IMAGES } from 'constants/howItWorks';
+import styles from '../styles/GetStarted.module.sass';
 import type { FC } from 'react';
+import { HOW_IT_WORKS_PATH } from 'constants/general';
 
 export const GetStarted: FC = () => {
   const images = GET_STARTED_IMAGES.map(({ src, alt, caption }) => {
@@ -19,17 +21,33 @@ export const GetStarted: FC = () => {
   });
 
   return (
-    <article>
-      <section>
-        <h2>Ready to get started?</h2>
-        <p>
-          Fill out your contest brief and begin receiving custom name
-          suggestions within minutes.
-        </p>
-        <Link to={'/startContest'}>Start A Contest</Link>
-      </section>
-      <section>{images}</section>
-    </article>
+    <>
+      <article className={styles.container}>
+        <figure className={styles.icon}>
+          <img
+            src={`${HOW_IT_WORKS_PATH}getStartedBG1.svg`}
+            alt="get started icon"
+          />
+        </figure>
+        <section className={styles.headingContainer}>
+          <h2 className={styles.getStartedHeading}>Ready to get started?</h2>
+          <p className={styles.getStartedText}>
+            Fill out your contest brief and begin receiving custom name
+            suggestions within minutes.
+          </p>
+          <Link className={styles.button} to={'/startContest'}>
+            Start A Contest
+          </Link>
+        </section>
+        <figure className={styles.icon}>
+          <img
+            src={`${HOW_IT_WORKS_PATH}getStartedBG2.svg`}
+            alt="get started icon"
+          />
+        </figure>
+      </article>
+      <article className={styles.container}>{images}</article>
+    </>
   );
 };
 
