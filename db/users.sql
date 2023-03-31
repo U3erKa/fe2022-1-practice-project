290fb43db1cf0eba40f295141fa226fda6f932fc
Viktor Stepanov <vitya.zap@gmail.com>
2023-03-31 13:28:44 +0300

    Select customer holiday orders' sums

--- db/users.sql
+++ db/users.sql
@@ -4,6 +4,15 @@ SELECT role,
 FROM "Users"
 GROUP BY role;
 
+-- @block Send 10% cashback for customers' orders during holidays
+SELECT "Users".id,
+  sum("Contests".prize)
+FROM "Users"
+  INNER JOIN "Contests" ON "Users".id = "Contests"."userId"
+WHERE role = 'customer'
+  AND "Contests"."createdAt" BETWEEN '2022-12-25' AND '2023-01-14'
+GROUP BY "Users".id;
+
 -- @block Add $10 for top 3 creators
 UPDATE "Users"
 SET balance = balance + 10
