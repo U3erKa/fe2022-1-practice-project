9c9ce15310828992ad087b7a9ad2e993259169cd
Viktor Stepanov <vitya.zap@gmail.com>
2023-04-02 19:25:47 +0300

    Style left side of ContactUs

--- client/src/pages/HowItWorksPage/components/ContactUs.tsx
+++ client/src/pages/HowItWorksPage/components/ContactUs.tsx
@@ -1,28 +1,37 @@
 import { Link } from 'react-router-dom';
 import { STATIC_IMAGES_PATH } from 'constants/general';
+import styles from '../styles/ContactUs.module.sass';
 import type { FC } from 'react';
 
 export const ContactUs: FC = () => {
   return (
-    <article>
-      <div>
-        <section>
-          <h3>Pay a Fraction of cost vs hiring an agency</h3>
-          <p>
-            For as low as $199, our naming contests and marketplace allow you to
-            get an amazing brand quickly and affordably.
-          </p>
-        </section>
-        <section>
-          <h3>Satisfaction Guarantee</h3>
-          <p>
-            Of course! We have policies in place to ensure that you are
-            satisfied with your experience.{' '}
-            <Link to={'/how-it-works#satisfaction'}>Learn more</Link>
-          </p>
-        </section>
-      </div>
-      <section>
+    <article className={styles.container}>
+      <ul className={styles.contentsContainer}>
+        <li className={styles.contents}>
+          <i className="fas fa-angle-right"></i>
+          <section>
+            <h3 className={styles.smallHeading}>
+              Pay a Fraction of cost vs hiring an agency
+            </h3>
+            <p className={styles.text}>
+              For as low as $199, our naming contests and marketplace allow you
+              to get an amazing brand quickly and affordably.
+            </p>
+          </section>
+        </li>
+        <li className={styles.contents}>
+          <i className="fas fa-angle-right"></i>
+          <section>
+            <h3 className={styles.bigHeading}>Satisfaction Guarantee</h3>
+            <p className={styles.text}>
+              Of course! We have policies in place to ensure that you are
+              satisfied with your experience.{' '}
+              <Link to={'/how-it-works#satisfaction'}>Learn more</Link>
+            </p>
+          </section>
+        </li>
+      </ul>
+      <section className={styles.contactsContainer}>
         <h3>Questions?</h3>
         <p>
           Speak with a Squadhelp platform expert to learn more and get your
