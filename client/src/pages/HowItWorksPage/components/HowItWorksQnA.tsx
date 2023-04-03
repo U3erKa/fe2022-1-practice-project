3b39a61bfad0999a99f1a6b7765ef253b94d8ed5
Viktor Stepanov <vitya.zap@gmail.com>
2023-04-03 12:47:47 +0300

    Style links container in HowItWorksQnA

--- client/src/pages/HowItWorksPage/components/HowItWorksQnA.tsx
+++ client/src/pages/HowItWorksPage/components/HowItWorksQnA.tsx
@@ -1,6 +1,7 @@
 import { uniqueId } from 'lodash';
 import { HOW_IT_WORKS_QNA } from 'constants/howItWorks';
 import { Answer } from '..';
+import styles from '../styles/HowItWorksQnA.module.sass';
 import type { FC } from 'react';
 
 export const HowItWorksQnA: FC = () => {
@@ -9,14 +10,14 @@ export const HowItWorksQnA: FC = () => {
 
   HOW_IT_WORKS_QNA.forEach(({ id, title, questions }) => {
     links.push(
-      <p key={id}>
+      <p className={styles.link} key={id}>
         <a href={`#${id}`}>{title}</a>
       </p>,
     );
 
     answers.push(
       <section id={id} key={id}>
-        <h2>{title}</h2>
+        <h2 className={styles.heading}>{title}</h2>
         {questions.map(([question, answer]) => {
           return (
             <div key={uniqueId()}>
@@ -30,9 +31,9 @@ export const HowItWorksQnA: FC = () => {
   });
 
   return (
-    <article>
-      <section>{links}</section>
-      {answers}
+    <article className={styles.container}>
+      <section className={styles.linksContainer}>{links}</section>
+      <section className={styles.answersContainer}>{answers}</section>
     </article>
   );
 };
