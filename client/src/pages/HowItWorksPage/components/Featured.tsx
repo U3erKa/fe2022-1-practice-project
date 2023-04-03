7ec85eb746a53db7f7f5e2b21184c9ac6a0b4977
Viktor Stepanov <vitya.zap@gmail.com>
2023-04-03 12:07:00 +0300

    Add basic styles for Featured

--- client/src/pages/HowItWorksPage/components/Featured.tsx
+++ client/src/pages/HowItWorksPage/components/Featured.tsx
@@ -1,17 +1,20 @@
 import { FEATURED } from 'constants/howItWorks';
+import styles from '../styles/Featured.module.sass';
 import type { FC } from 'react';
 
 export const Featured: FC = () => {
   const featuredLinks = FEATURED.map(({ src, alt, href }) => (
-    <a key={alt} href={href}>
-      <img src={src} alt={alt} />
-    </a>
+    <p key={alt} className={styles.link}>
+      <a href={href}>
+        <img src={src} alt={alt} />
+      </a>
+    </p>
   ));
 
   return (
-    <article>
-      <h2>Featured In</h2>
-      {featuredLinks}
+    <article className={styles.container}>
+      <h2 className={styles.heading}>Featured In</h2>
+      <section className={styles.linksContainer}>{featuredLinks}</section>
     </article>
   );
 };
