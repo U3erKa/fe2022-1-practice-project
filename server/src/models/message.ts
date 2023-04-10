52deb299dbbf09aa2bf9b337de38758b22de3a39
Viktor Stepanov <vitya.zap@gmail.com>
2023-04-10 10:28:39 +0300

    Define Message model

---
+++ server/src/models/message.ts
@@ -0,0 +1,56 @@
+import { Model } from 'sequelize';
+// prettier-ignore
+import type { 
+  DataTypes as _DataTypes, InferAttributes, InferCreationAttributes,
+  CreationOptional, ForeignKey,
+} from 'sequelize';
+import type { Conversation, DB, User } from '../types/models';
+
+const Message = (sequelize: DB['sequelize'], DataTypes: typeof _DataTypes) => {
+  class Message extends _Message {
+    static associate(models: DB) {}
+  }
+  Message.init(
+    {
+      _id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      body: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      createdAt: {
+        type: DataTypes.DATE,
+        allowNull: true,
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+        allowNull: true,
+      },
+    },
+    {
+      sequelize,
+      modelName: 'Message',
+      timestamps: true,
+    },
+  );
+  return Message;
+};
+
+abstract class _Message extends Model<
+  InferAttributes<_Message>,
+  InferCreationAttributes<_Message>
+> {
+  declare body: string;
+
+  declare _id: CreationOptional<number>;
+  declare createdAt: CreationOptional<Date>;
+  declare updatedAt: CreationOptional<Date>;
+  declare sender: ForeignKey<User['id']>;
+  declare conversation: ForeignKey<Conversation['_id']>;
+}
+
+export = Message;
