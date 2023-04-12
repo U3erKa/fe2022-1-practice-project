a3e531eb3db66bb173ed40690b93e6d429c4de7d
Viktor Stepanov <vitya.zap@gmail.com>
2023-04-12 20:07:54 +0300

    Create migration for catalogs_to_conversations

---
+++ server/src/migrations/mn20230412142334-create-catalogs_to_conversations.js
@@ -0,0 +1,29 @@
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('catalogs_to_conversations', {
+      catalogId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        references: {
+          model: 'catalogs',
+          key: '_id',
+        },
+      },
+      conversationId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        references: {
+          model: 'conversations',
+          key: '_id',
+        },
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('catalogs_to_conversations');
+  },
+};
