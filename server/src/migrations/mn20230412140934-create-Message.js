fa16270bd70124e4a738a251a9192b9e5574fcb4
Viktor Stepanov <vitya.zap@gmail.com>
2023-04-12 20:07:29 +0300

    Create migration for Message

---
+++ server/src/migrations/mn20230412140934-create-Message.js
@@ -0,0 +1,46 @@
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('messages', {
+      _id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      conversation: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          key: '_id',
+          model: 'conversations',
+        },
+      },
+      sender: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        unique: true,
+        references: {
+          key: 'id',
+          model: 'Users',
+        },
+      },
+      body: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('messages');
+  },
+};
