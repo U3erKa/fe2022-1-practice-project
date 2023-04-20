3f5972eceed49a401a15af8a75cca77f89bdb93f
Viktor Stepanov <vitya.zap@gmail.com>
2023-04-20 16:03:59 +0300

    Fix migration for catalogs_to_conversations

--- server/src/migrations/mn20230412142334-create-catalogs_to_conversations.js
+++ server/src/migrations/mn20230412142334-create-catalogs_to_conversations.js
@@ -20,6 +20,14 @@ module.exports = {
           key: '_id',
         },
       },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
     });
   },
 
