307de009e2640a17d5bd25d653ac736b4078e365
Viktor Stepanov <vitya.zap@gmail.com>
2023-06-19 21:06:12 +0300

    Fix migration unique key constraint

--- server/src/migrations/mn20230412140918-create-Conversation.js
+++ server/src/migrations/mn20230412140918-create-Conversation.js
@@ -1,48 +1,56 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('conversations', {
-      _id: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true,
-      },
-      participant1: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        unique: true,
-        references: {
-          model: 'Users',
-          key: 'id',
+    await queryInterface.createTable(
+      'conversations',
+      {
+        _id: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          primaryKey: true,
+          autoIncrement: true,
+        },
+        participant1: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          references: {
+            model: 'Users',
+            key: 'id',
+          },
+        },
+        participant2: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          references: {
+            model: 'Users',
+            key: 'id',
+          },
+        },
+        favoriteList: {
+          type: Sequelize.ARRAY(Sequelize.BOOLEAN),
+          allowNull: false,
+          defaultValue: [false, false],
+        },
+        blackList: {
+          type: Sequelize.ARRAY(Sequelize.BOOLEAN),
+          allowNull: false,
+          defaultValue: [false, false],
+        },
+        createdAt: {
+          type: Sequelize.DATE,
+          allowNull: false,
+        },
+        updatedAt: {
+          type: Sequelize.DATE,
+          allowNull: false,
         },
       },
-      participant2: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        unique: true,
-        references: {
-          model: 'Users',
-          key: 'id',
+      {
+        uniqueKeys: {
+          participants: { fields: ['participant1', 'participant2'] },
         },
       },
-      favoriteList: {
-        type: Sequelize.ARRAY(Sequelize.BOOLEAN),
-        allowNull: false,
-      },
-      blackList: {
-        type: Sequelize.ARRAY(Sequelize.BOOLEAN),
-        allowNull: false,
-      },
-      createdAt: {
-        type: Sequelize.DATE,
-        allowNull: false,
-      },
-      updatedAt: {
-        type: Sequelize.DATE,
-        allowNull: false,
-      },
-    });
+    );
   },
 
   async down(queryInterface, Sequelize) {
