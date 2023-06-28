bbe38d2e1d863187661b9c539162b359d2c47c30
Viktor Stepanov <vitya.zap@gmail.com>
2023-06-28 20:52:55 +0300

    Add styles

--- client/src/pages/StartContestPage/components/ButtonGroup.tsx
+++ client/src/pages/StartContestPage/components/ButtonGroup.tsx
@@ -2,6 +2,7 @@ import cx from 'classnames';
 import { BUTTONS, NAME_ONLY_CHOISE } from 'constants/buttonGroup';
 
 import styles from '../styles/ButtonGroup.module.sass';
+
 export default function ButtonGroup() {
   const buttons = BUTTONS.map(({ choise, text }) => (
     <button
