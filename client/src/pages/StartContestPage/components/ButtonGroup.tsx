8ac10ee50c0f93011531dd9af1b95d2c8f277a9f
Viktor Stepanov <vitya.zap@gmail.com>
2023-06-28 20:53:41 +0300

    Store chosen button

--- client/src/pages/StartContestPage/components/ButtonGroup.tsx
+++ client/src/pages/StartContestPage/components/ButtonGroup.tsx
@@ -1,15 +1,22 @@
 import cx from 'classnames';
+import { useSelector, useDispatch } from 'hooks';
+import { changeNameMathesDomain } from 'store/slices/contestCreationSlice';
 import { BUTTONS, NAME_ONLY_CHOISE } from 'constants/buttonGroup';
-
 import styles from '../styles/ButtonGroup.module.sass';
 
 export default function ButtonGroup() {
+  const { nameMathesDomain } = useSelector(
+    ({ contestCreationStore }) => contestCreationStore,
+  );
+  const dispatch = useDispatch();
+
   const buttons = BUTTONS.map(({ choise, text }) => (
     <button
       className={cx({
         [styles.button]: true,
         [styles.active]: nameMathesDomain === choise,
       })}
+      onClick={() => dispatch(changeNameMathesDomain(choise))}
     >
       <h3 className={styles.choise}>
         {choise !== NAME_ONLY_CHOISE ? 'Yes' : 'No'}
--- client/src/store/slices/contestCreationSlice.ts
+++ client/src/store/slices/contestCreationSlice.ts
@@ -1,13 +1,15 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { SIMILAR_CHOISE } from 'constants/buttonGroup';
 
 import type { PayloadAction } from '@reduxjs/toolkit';
 import type { SaveContestToStore } from 'types/api/contest';
-import type { ContestCreationState } from 'types/slices';
+import type { ContestCreationState, NameMatchesDomain } from 'types/slices';
 
 const CONTEST_SAVING_SLICE_NAME = 'contestCreation';
 
 const initialState: ContestCreationState = {
   contests: {},
+  nameMathesDomain: SIMILAR_CHOISE,
 };
 
 const reducers = {
@@ -20,6 +22,12 @@ const reducers = {
       ...{ [type]: info },
     };
   },
+  changeNameMathesDomain: (
+    state: ContestCreationState,
+    { payload }: PayloadAction<NameMatchesDomain>,
+  ) => {
+    state.nameMathesDomain = payload;
+  },
   clearContestStore: () => initialState,
 };
 
@@ -31,6 +39,7 @@ const contestSavingSlice = createSlice({
 
 const { actions, reducer } = contestSavingSlice;
 
-export const { saveContestToStore, clearContestStore } = actions;
+export const { saveContestToStore, changeNameMathesDomain, clearContestStore } =
+  actions;
 
 export default reducer;
