6582d57b3b6a0219f8b84a0d49b19c7ac3e648bc
Viktor Stepanov <vitya.zap@gmail.com>
2023-06-30 11:35:22 +0300

    Fix non-unique id of buttons

--- client/src/pages/StartContestPage/components/ButtonGroup.tsx
+++ client/src/pages/StartContestPage/components/ButtonGroup.tsx
@@ -3,6 +3,7 @@ import { useSelector, useDispatch } from 'hooks';
 import { changeNameMathesDomain } from 'store/slices/contestCreationSlice';
 import { BUTTONS, NAME_ONLY_CHOISE } from 'constants/buttonGroup';
 import styles from '../styles/ButtonGroup.module.sass';
+import { uniqueId } from 'lodash';
 
 export default function ButtonGroup() {
   const { nameMathesDomain } = useSelector(
@@ -12,6 +13,7 @@ export default function ButtonGroup() {
 
   const buttons = BUTTONS.map(({ choise, text }) => (
     <button
+      key={uniqueId('button')}
       className={cx({
         [styles.button]: true,
         [styles.active]: nameMathesDomain === choise,
