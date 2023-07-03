8940b600abf2b853e920fef08c8a24c2fedce7e6
Viktor Stepanov <vitya.zap@gmail.com>
2023-07-03 14:20:10 +0300

    Fix EventsList import

--- client/src/pages/EventsPage/index.ts
+++ client/src/pages/EventsPage/index.ts
@@ -1,2 +1,3 @@
 export { default as CreateEvent } from './components/CreateEvent';
+export { default as EventsList } from './components/EventsList';
 export { default as EventsListItems } from './components/EventsListItems';
