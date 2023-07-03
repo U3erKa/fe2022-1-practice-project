77f849a04b24aeee5e369da2d2d279e2ff0b03fb
Viktor Stepanov <vitya.zap@gmail.com>
2023-07-03 14:18:58 +0300

    Add basic EventsList

---
+++ client/src/pages/EventsPage/components/EventsList.tsx
@@ -0,0 +1,16 @@
+import { EventsListItems } from '..';
+
+export default function EventsList() {
+  return (
+    <article>
+      <section>
+        <h2>Live upcoming checks</h2>
+        <p>
+          <span>Remaining time</span>
+          <i className="fas fa-user-clock"></i>
+        </p>
+      </section>
+      <EventsListItems />
+    </article>
+  );
+}
