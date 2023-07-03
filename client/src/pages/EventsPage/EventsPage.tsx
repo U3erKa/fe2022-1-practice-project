bc5c662ed5c0ba236d014bcdb8ecc096859df194
Viktor Stepanov <vitya.zap@gmail.com>
2023-07-03 14:54:20 +0300

    Connect EventsList component

--- client/src/pages/EventsPage/EventsPage.tsx
+++ client/src/pages/EventsPage/EventsPage.tsx
@@ -1,11 +1,12 @@
 import { Footer, Header } from 'components/general';
-import { CreateEvent } from '.';
+import { CreateEvent, EventsList } from '.';
 
 export default function EventsPage() {
   return (
     <>
       <Header />
       <h1>My Events</h1>
+      <EventsList />
       <CreateEvent />
       <Footer />
     </>
