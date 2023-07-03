65a936fccdc8599aaf12b38e7459170528304d63
Viktor Stepanov <vitya.zap@gmail.com>
2023-07-03 14:53:52 +0300

    Dynamically add progress prop to events

--- client/src/pages/EventsPage/components/EventsList.tsx
+++ client/src/pages/EventsPage/components/EventsList.tsx
@@ -1,6 +1,35 @@
+import { useEffect } from 'react';
+import { useForceUpdate } from 'hooks';
+import { events } from './EventsListItems';
 import { EventsListItems } from '..';
 
+function addProgressToEvents() {
+  const currentDate = Date.now();
+  events.forEach((event) => {
+    const { date, createdAt } = event;
+    const plannedDate = new Date(date).valueOf();
+    const createdAtDate = new Date(createdAt).valueOf();
+    const createdAtTimeframe = plannedDate - createdAtDate;
+    const currentDateTimeframe = plannedDate - currentDate;
+    
+    const progress = 1 - currentDateTimeframe / createdAtTimeframe;
+    Object.assign(event, { progress: Math.min(progress, 1) });
+  });
+}
+
 export default function EventsList() {
+  useEffect(() => {
+    const interval = setInterval(() => {
+      addProgressToEvents();
+      forceUpdate();
+    }, 1000);
+
+    return () => clearInterval(interval);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const forceUpdate = useForceUpdate();
+
   return (
     <article>
       <section>
