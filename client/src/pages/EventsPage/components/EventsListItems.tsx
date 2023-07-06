4c1a879e3d01a5455fd807c0bc04c9a7f0e66001
Viktor Stepanov <vitya.zap@gmail.com>
2023-07-06 22:17:11 +0300

    Reimplement getting event progress properly

--- client/src/pages/EventsPage/components/EventsListItems.tsx
+++ client/src/pages/EventsPage/components/EventsListItems.tsx
@@ -34,10 +34,42 @@ export const events: (typeof initialValues & {
   },
 ];
 
+const getDays = (time: number) => Math.floor(time / (1000 * 60 * 60 * 24));
+const getHours = (time: number) => Math.floor((time / (1000 * 60 * 60)) % 24);
+const getMinutes = (time: number) => Math.floor((time / (1000 * 60)) % 60);
+const getSeconds = (time: number) => Math.floor((time / 1000) % 60);
+
+const getRemainingTime = (time: number) => {
+  const days = getDays(time);
+  const hours = getHours(time);
+  const minutes = getMinutes(time);
+  const seconds = getSeconds(time);
+  const result: string[] = [];
+
+  if (days > 0) result.push(`${days}d`);
+  if (hours > 0) result.push(`${hours}h`);
+  if (minutes > 0) result.push(`${minutes}m`);
+  if (seconds > 0) result.push(`${seconds}s`);
+  return result.join(' ') || '0s';
+};
+
+function getEventProgress({ date, createdAt }: (typeof events)[number]) {
+  const currentDate = Date.now();
+  const plannedDate = Date.parse(date);
+  const createdAtDate = Date.parse(createdAt);
+  const createdAtTimeframe = plannedDate - createdAtDate;
+  const currentDateTimeframe = plannedDate - currentDate;
+  const progressValue = 1 - currentDateTimeframe / createdAtTimeframe;
+
+  const progress = Math.min(isNaN(progressValue) ? 0 : progressValue, 1);
+  const time = getRemainingTime(currentDateTimeframe);
+  return { progress, time };
+}
+
 const closestEventFirst = (
   { date }: (typeof events)[number],
   { date: other }: (typeof events)[number],
-) => new Date(date).valueOf() - new Date(other).valueOf();
+) => Date.parse(date) - Date.parse(other);
 
 export default function EventsListItems() {
   const eventsList = events
