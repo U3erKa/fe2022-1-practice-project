073925c4c238de54548d21da4b9465559c9591ff
Viktor Stepanov <vitya.zap@gmail.com>
2023-07-13 20:04:41 +0300

    Add styles for EventsList

--- client/src/pages/EventsPage/components/EventsList.tsx
+++ client/src/pages/EventsPage/components/EventsList.tsx
@@ -1,12 +1,12 @@
 import { EventListItems } from '..';
-
+import styles from '../styles/EventsList.module.sass';
 
 export default function EventsList() {
   return (
-    <article>
-      <section>
-        <h2>Live upcoming checks</h2>
-        <p>
+    <article className={styles.container}>
+      <section className={styles.headingContainer}>
+        <h1 className={styles.heading}>Live upcoming checks</h1>
+        <p className={styles.textContainer}>
           <span>Remaining time</span>
           <i className="fas fa-user-clock"></i>
         </p>
