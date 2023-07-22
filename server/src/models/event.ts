c3245af9dea4f431b1a9974cd45e638fc7179883
Viktor Stepanov <vitya.zap@gmail.com>
2023-07-22 12:22:04 +0300

    Create Event model

---
+++ server/src/models/event.ts
@@ -0,0 +1,72 @@
+'use strict';
+import { Model } from 'sequelize';
+// prettier-ignore
+import type { 
+  DataTypes as _DataTypes, InferAttributes, InferCreationAttributes, CreationOptional,
+  NonAttribute, ForeignKey, Association,
+  BelongsToCreateAssociationMixin, BelongsToGetAssociationMixin, BelongsToSetAssociationMixin,
+} from 'sequelize';
+import type { DB, User } from '../types/models';
+
+const Event = (sequelize: DB['sequelize'], DataTypes: typeof _DataTypes) => {
+  class Event extends _Event {
+    static associate({ User }: DB) {
+    }
+  }
+  Event.init(
+    {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      name: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      date: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+      notify: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      userId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+    },
+    {
+      sequelize,
+      modelName: 'Event',
+      timestamps: true,
+    },
+  );
+  return Event;
+};
+
+abstract class _Event extends Model<
+  InferAttributes<_Event>,
+  InferCreationAttributes<_Event>
+> {
+  declare name: string;
+  declare date: Date;
+  declare notify: string;
+
+  declare id: CreationOptional<number>;
+  declare userId: ForeignKey<User['id']>;
+
+  declare user?: NonAttribute<DB['User'][]>;
+
+  declare static associations: {
+    user: Association<DB['Offer'] & Model, DB['User'] & Model>;
+  };
+
+  declare createUser: BelongsToCreateAssociationMixin<DB['User'] & Model>;
+  declare getUser: BelongsToGetAssociationMixin<DB['User']>;
+  declare addUser: BelongsToSetAssociationMixin<DB['User'], number>;
+}
+
+export = Event;
