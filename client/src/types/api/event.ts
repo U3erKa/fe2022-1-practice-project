a0cafb8c555aaf71935da09f779ae80c3668c69c
Viktor Stepanov <vitya.zap@gmail.com>
2023-07-24 19:00:32 +0300

    Fix types for events

--- client/src/types/api/event.ts
+++ client/src/types/api/event.ts
@@ -2,8 +2,8 @@ import type { InferType } from 'yup';
 import type { newEventSchema } from 'utils/validators/validationSchems';
 import type { UserId, WithId, WithTimeStamps } from './_common';
 
-export type GetEventsResponse = WithId<UserId> &
-  WithTimeStamps & { name: string; date: Date; notify: string };
+export type GetEventsResponse = CreateEventResponse[];
 
 export type CreateEventRequest = InferType<typeof newEventSchema>;
-export type CreateEventResponse = GetEventsResponse;
+export type CreateEventResponse = WithId<UserId> &
+WithTimeStamps & { name: string; date: Date; notify: string };
