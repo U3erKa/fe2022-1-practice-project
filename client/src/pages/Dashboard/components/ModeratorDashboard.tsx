91c523cda182ef0f52ba392246c3733153bee958
Viktor Stepanov <vitya.zap@gmail.com>
2023-08-13 15:35:18 +0300

    Add layout for ModeratorDashboard

--- client/src/pages/Dashboard/components/ModeratorDashboard.tsx
+++ client/src/pages/Dashboard/components/ModeratorDashboard.tsx
@@ -1,5 +1,45 @@
+import { TryAgain, ItemsContainer } from 'components/general';
+import { CustomFilter } from './CustomFilter';
+import styles from '../styles/CustomerDashboard.module.sass';
+
+const buttons = [
+  { name: 'Offers to review', filter: false },
+  { name: 'Reviewed offers', filter: true },
+];
+
 export default function ModeratorDashboard() {
+  const { isFetching, haveMore, error, offers, isReviewed, contestData } =
+    useSelector(({ contestByIdStore }) => contestByIdStore);
+  const items = offers.map((offer) => (
+    <OfferBox
+      data={offer}
+      key={offer.id}
+      setOfferStatus={setOfferStatusMethod}
+      contestData={contestData}
+    />
+  ));
+
   return (
-    <div>ModeratorDashboard</div>
-  )
+    <div className={styles.mainContainer}>
+      <CustomFilter
+        buttons={buttons}
+        predicate={isReviewed}
+        filterAction={
+          setOfferReviewStatus as ActionCreatorWithPayload<any, string>
+        }
+      />
+      <div className={styles.contestsContainer}>
+        {error ? (
+          <TryAgain getData={getOffersMethod} />
+        ) : (
+          <ItemsContainer
+            isFetching={isFetching}
+            haveMore={haveMore}
+            items={items}
+            loadMore={getOffersMethod}
+          />
+        )}
+      </div>
+    </div>
+  );
 }
