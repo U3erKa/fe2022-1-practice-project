dc4d210ef3cc49d44572182f8bc7365b042d54aa
Viktor Stepanov <vitya.zap@gmail.com>
2023-08-31 04:09:34 +0300

    Refactor EventsPage
    Filter events in a single iteration

--- client/src/pages/EventsPage/EventsPage.tsx
+++ client/src/pages/EventsPage/EventsPage.tsx
@@ -1,15 +1,28 @@
+import { fork } from 'radash';
+import { useForceUpdate, useSelector } from 'hooks';
 import { Footer, Header } from 'components/general';
-import { useForceUpdate } from 'hooks';
 import { CreateEvent, EventsList } from '.';
+import { getRemainingTime } from 'utils/functions';
 
 export default function EventsPage() {
+  const { events } = useSelector(({ events }) => events);
   const forceUpdate = useForceUpdate();
+
+  const currentDate = Date.now();
+  const [pastEvents, upcomingEvents] = fork(events, ({ date }) => {
+    const plannedDate = Date.parse(date);
+    const timeframe = plannedDate - currentDate;
+    const eventIsPast = getRemainingTime(timeframe) === '0s';
+
+    return eventIsPast;
+  });
+
   return (
     <>
       <Header />
-      <EventsList />
+      <EventsList events={upcomingEvents} />
       <CreateEvent forceUpdate={forceUpdate} />
-      <EventsList isPast={true} />
+      <EventsList isPast={true} events={pastEvents} />
       <Footer />
     </>
   );
--- client/src/pages/EventsPage/components/EventListItems.tsx
+++ client/src/pages/EventsPage/components/EventListItems.tsx
@@ -1,29 +1,19 @@
 import { useSelector } from 'hooks';
 import { Spinner } from 'components/general';
 import { Event } from '..';
-import { getRemainingTime } from 'utils/functions';
 import styles from '../styles/EventListItems.module.sass';
 
-export default function EventListItems({ isPast = false }) {
-  const { isFetching, events } = useSelector(({ events }) => events);
+export default function EventListItems({ events }) {
+  const { isFetching } = useSelector(({ events }) => events);
 
   if (isFetching) return <Spinner />;
 
-  const currentDate = Date.now();
-  const filteredEvents = events.filter(({ date }) => {
-    const plannedDate = Date.parse(date);
-    const timeframe = plannedDate - currentDate;
-    const eventIsPast = (getRemainingTime(timeframe) === '0s') === isPast;
-
-    return eventIsPast;
-  });
-
-  if (!filteredEvents?.length)
+  if (!events?.length)
     return <section>No events here yet. Feel Free to create one!</section>;
 
   return (
     <article className={styles.container}>
-      {filteredEvents.map((event) => (
+      {events.map((event) => (
         <Event key={event.id} {...event} />
       ))}
     </article>
