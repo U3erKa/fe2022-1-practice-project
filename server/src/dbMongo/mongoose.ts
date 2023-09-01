65b99e336504219fabbe8975c7097710f8395e36
Viktor Stepanov <vitya.zap@gmail.com>
2023-09-01 15:22:15 +0300

    Add back config params for mongoose connection

--- server/src/dbMongo/mongoose.ts
+++ server/src/dbMongo/mongoose.ts
@@ -5,7 +5,11 @@ const configPath = path.join(__dirname, '..', 'config/mongoConfig.json');
 const config = require(configPath)[env];
 
 mongoose
-  .connect(`mongodb://${config.host}:${config.port}/${config.database}`)
+  .connect(`mongodb://${config.host}:${config.port}/${config.database}`, {
+    // @ts-expect-error
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .catch((err) => {
     if (err) {
       console.log(err);
