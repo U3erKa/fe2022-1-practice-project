355c499d459fb827ff7ef2bf207d3a33d9d1d0d2
Viktor Stepanov <vitya.zap@gmail.com>
2023-09-15 20:51:21 +0300

    Add back support for browserslist

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import browserslistToEsbuild from 'browserslist-to-esbuild';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: { target: browserslistToEsbuild() },
   plugins: [react(), tsconfigPaths()],
 });
