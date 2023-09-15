25b9c79e36fb5639b047d0b2b47b6e336b433285
Viktor Stepanov <vitya.zap@gmail.com>
2023-09-15 19:03:15 +0300

    Address breaking changes for react-spinners

--- client/src/components/general/Spinner.tsx
+++ client/src/components/general/Spinner.tsx
@@ -1,18 +1,11 @@
 import { ClipLoader } from 'react-spinners';
-import { css } from '@emotion/core';
 
 import styles from './styles/Spinner.module.sass';
-
-const override = css`
-  border-color: #46568a;
-`;
 
 const SpinnerLoader = () => (
   <div className={styles.loaderContainer}>
     <ClipLoader
-      // @ts-expect-error
-      sizeUnit="px"
-      css={override}
+      cssOverride={{ borderColor: '#46568a' }}
       size={50}
       color="#46568a"
       loading
