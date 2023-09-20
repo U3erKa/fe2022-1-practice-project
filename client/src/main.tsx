410263070b7feadb567c582eccf90714c11f1554
Viktor Stepanov <vitya.zap@gmail.com>
2023-09-20 21:29:53 +0300

    Use vite-plugin-pwa plugin

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 
-import * as serviceWorker from './serviceWorker';
 import store from './store';
 import App from './App';
 import './index.css';
@@ -14,8 +13,3 @@ createRoot(document.getElementById('root') as HTMLDivElement).render(
     </Provider>
   </StrictMode>,
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,10 +2,15 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import browserslistToEsbuild from 'browserslist-to-esbuild';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: { target: browserslistToEsbuild() },
   css: { devSourcemap: true },
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    VitePWA({ devOptions: { enabled: true }, registerType: 'autoUpdate' }),
+  ],
 });
