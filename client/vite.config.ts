61cff498a3cb6fc170afbf33b987d991c69405e5
Viktor Stepanov <vitya.zap@gmail.com>
2023-09-20 22:56:03 +0300

    Update manifest

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,11 +6,15 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: { target: browserslistToEsbuild() },
+  build: { target: browserslistToEsbuild(), sourcemap: true },
   css: { devSourcemap: true },
   plugins: [
     react(),
     tsconfigPaths(),
-    VitePWA({ devOptions: { enabled: true }, registerType: 'autoUpdate' }),
+    VitePWA({
+      devOptions: { enabled: true },
+      registerType: 'autoUpdate',
+      manifest: false,
+    }),
   ],
 });
