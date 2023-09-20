7fd05bad089db07a5f7136df6697fc2f579b0c15
Viktor Stepanov <vitya.zap@gmail.com>
2023-09-20 21:01:15 +0300

    Add sourcemaps for css

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,5 +6,6 @@ import browserslistToEsbuild from 'browserslist-to-esbuild';
 // https://vitejs.dev/config/
 export default defineConfig({
   build: { target: browserslistToEsbuild() },
+  css: { devSourcemap: true },
   plugins: [react(), tsconfigPaths()],
 });
