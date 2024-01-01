e9a2d3f59951c71200c3f677b1eb3c31705adda3
Viktor Stepanov <vitya.zap@gmail.com>
2024-01-01 20:45:44 +0200

    types: add types to infer API handler's return

--- src/types/_common.ts
+++ src/types/_common.ts
@@ -1,3 +1,11 @@
 import type { AppRouterInstance } from 'next/dist/shared/lib/app-router-context.shared-runtime';
+import type { NextResponse } from 'next/server';
 
 export type WithNavigate = { navigate: AppRouterInstance['push' | 'replace'] };
+
+export type APIReturn<T extends Promise<NextResponse>> =
+  Awaited<T> extends NextResponse<infer R> ? R : never;
+
+export type APIHandlerReturn<
+  T extends (...args: any[]) => Promise<NextResponse>,
+> = APIReturn<ReturnType<T>>;
