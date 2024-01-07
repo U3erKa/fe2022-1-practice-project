35bd6cd0b17784255fde631008db2b2138542ad2
Viktor Stepanov <vitya.zap@gmail.com>
2024-01-07 11:06:39 +0200

    refactor: extract repeating logic to function

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -2,42 +2,38 @@ import { RefreshToken } from 'models';
 import type { User, RefreshToken as _RefreshToken } from 'types/models';
 import { generateTokenPair } from './jwtService';
 
-export const createSession = async (user: User) => {
-  const tokenPair = await generateTokenPair({
-    avatar: user.avatar,
-    balance: user.balance,
-    displayName: user.displayName,
-    email: user.email,
-    firstName: user.firstName,
-    lastName: user.lastName,
-    rating: user.rating,
-    role: user.role,
-    userId: user.id,
-  });
+export const sanitizeUserData = ({
+  avatar,
+  balance,
+  displayName,
+  email,
+  firstName,
+  lastName,
+  rating,
+  role,
+  id: userId,
+}: User) => ({
+  avatar,
+  balance,
+  displayName,
+  email,
+  firstName,
+  lastName,
+  rating,
+  role,
+  userId,
+});
 
-  await RefreshToken.create({
-    token: tokenPair.refreshToken,
-    userId: user.id,
-  });
+export const createSession = async (user: User) => {
+  const tokenPair = await generateTokenPair(sanitizeUserData(user));
+  await RefreshToken.create({ token: tokenPair.refreshToken, userId: user.id });
 
   return { tokenPair, user };
 };
 
 export const refreshSession = async (refreshTokenInstance: _RefreshToken) => {
   const user = await refreshTokenInstance.getUser();
-
-  const tokenPair = await generateTokenPair({
-    avatar: user.avatar,
-    balance: user.balance,
-    displayName: user.displayName,
-    email: user.email,
-    firstName: user.firstName,
-    lastName: user.lastName,
-    rating: user.rating,
-    role: user.role,
-    userId: user.id,
-  });
-
+  const tokenPair = await generateTokenPair(sanitizeUserData(user));
   await refreshTokenInstance.update({ token: tokenPair.refreshToken });
 
   return { tokenPair, user };
