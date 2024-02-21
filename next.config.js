44aad6b381de6beac17253361cba4522d2db5af7
Viktor Stepanov <vitya.zap@gmail.com>
2024-02-21 18:37:00 +0200

    config: enable @next/bundle-analyzer

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
+import bundleAnalyzer from '@next/bundle-analyzer';
+
+const withBundleAnalyzer = bundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -6,4 +12,4 @@ const nextConfig = {
   reactStrictMode: true,
 };
 
-export default nextConfig;
+export default withBundleAnalyzer(nextConfig);
